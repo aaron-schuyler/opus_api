bc270a492da4c14acde861012145418631ed3ce6
aaron-schuyler <asky28@gmail.com>
2020-08-18 19:10:43 -0400

    update action for folders

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -7,19 +7,25 @@ class FoldersController < ApplicationController
   end
   def create
     folder = current_user.folders.new(folder_params)
-    if folder.valid?
-      folder.save
-      render json: {success: true, message: "Folder '#{folder.name}' saved successfuly."}
-    else
-      render json: {success: false, messages: folder.errors.full_messages}
-    end
+    save_folder folder
   end
   def update
+    folder = Folder.find_by(id: params[:id], user: current_user)
+    folder.assign_attributes(folder_params)
+    save_folder folder
   end
   def destroy
   end
   private
   def folder_params
     params.require(:folder).permit(:name, :color)
+  end
+  def save_folder(folder)
+    if folder.valid?
+      folder.save
+      render json: {success: true, message: "Folder '#{folder.name}' saved successfuly."}
+    else
+      render json: {success: false, messages: folder.errors.full_messages}
+    end
   end
 end
